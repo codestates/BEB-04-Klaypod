fbd19b7b76344988267e60e57f188c84edcc441a
wavescats <wavescats@gmail.com>
2022-07-17 22:08:45 +0900

    [FEAT] 메인페이지 목업

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,17 @@
+import { Route, Routes, BrowserRouter as Router } from "react-router-dom";
 import "./App.css";
+import Main from "./components/Main";
 
 function App() {
-  return <div>123</div>;
+  return (
+    <>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Main />} />
+        </Routes>
+      </Router>
+    </>
+  );
 }
 
 export default App;
---
+++ client/src/components/Main.tsx
@@ -0,0 +1,100 @@
+import MainNav from "./MainNav";
+import MainPage from "../pages/MainPage";
+import MainFooter from "./MainFooter";
+import "../styles/Main.css";
+
+function Main() {
+  return (
+    <>
+      <MainNav />
+      <div className="main_page_slide">
+        <div className="main_wrapper clearfix">
+          <div className="Main_gif">
+            <div className="shadow"></div>
+            <div className="bowl">
+              <div className="liquid"></div>
+            </div>
+            <MainPage />
+          </div>
+          <div className="main_page_one">
+            <div className="main_page_one_title">KlayPod 에서 스캔하는 DEX</div>
+            <img
+              className="main_page_one_img"
+              src="https://gateway.pinata.cloud/ipfs/QmYATGeFczH5zSFtqWNrZAbXMRsRPVHXVeFuRJZwnXKwDC"
+              alt=""
+            />
+          </div>
+          <section className="main_page_two">
+            <li>
+              <img
+                className="main_page_two_img"
+                src="https://gateway.pinata.cloud/ipfs/QmX77ydLWPeQiX5EP4hYDcP88obd8FPoGJ6zy336uLchFo"
+                alt=""
+                width="250"
+                height="220"
+              />
+              <span className="main_page_two_title">DeFi SCAN</span>
+              <span className="main_page_two_text">
+                KlayPod 에서 지원하는
+                <br />
+                DEX 스캔을 통해 수익을 극대화 하세요!
+              </span>
+            </li>
+            <li>
+              <img
+                className="main_page_two_img"
+                src="https://gateway.pinata.cloud/ipfs/QmdMZoxTfKAJCKiyqPLiNPFGCr2jnXx1ktxiys2BfQNSbZ"
+                alt=""
+                width="250"
+                height="220"
+              />
+              <span className="main_page_two_title">Token Swap</span>
+              <span className="main_page_two_text">
+                Klaytn 코인으로 KPD 토큰을 스왑하여
+                <br />
+                KlayPod의 소유자가 되세요!
+              </span>
+            </li>
+            <li>
+              <img
+                className="main_page_two_img"
+                src="https://gateway.pinata.cloud/ipfs/QmfQcwxrQkwBM5B6BDkWx8Mm2Azanqpkgx66t31FBojyyS"
+                alt=""
+                width="250"
+                height="220"
+              />
+              <span className="main_page_two_title">Token Staking</span>
+              <span className="main_page_two_text">
+                KlayPod의 KPD 토큰을 홀드하세요.
+                <br />
+                스테이킹하여 수익을 얻으며
+                <br />
+                KlayPod의 가치를 높여보세요!
+              </span>
+            </li>
+            <li>
+              <img
+                className="main_page_two_img"
+                src="https://gateway.pinata.cloud/ipfs/QmVeqE7nTZfu8giC6RYjnkFuDPVvYEXgzfY36pta3m42ip"
+                alt=""
+                width="250"
+                height="220"
+              />
+              <span className="main_page_two_title">KlayPod Governance</span>
+              <span className="main_page_two_text">
+                KlayPod의 DAO 참여자가 되세요.
+                <br />
+                KPD 토큰으로 거버넌스 권한을 얻으세요!
+              </span>
+            </li>
+          </section>
+          <section className="main_page_three">
+            <MainFooter />
+          </section>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default Main;
